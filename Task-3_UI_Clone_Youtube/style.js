99d47330ad8c4bd8ea4682d3abbaf7058aef386b
navya2007-art <vnnavya2007@gmail.com>
2026-05-26 18:47:14 +0530

    Delete Task-3_UI_Clone_Youtube directory

--- Task-3_UI_Clone_Youtube/style.js
+++
@@ -1,9 +0,0 @@
-function searchVideo() {
-  let input = document.getElementById("searchInput").value;
-
-  if(input === "") {
-    alert("Please enter something to search");
-  } else {
-    alert("You searched for: " + input);
-  }
-}
